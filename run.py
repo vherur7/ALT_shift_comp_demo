b0aa490d8321f0b34f407d58858cd8733d907b59
Vivek Herur <vivekherur@viveks-mbp.mynetworksettings.com>
2023-03-28 18:55:43 -0400

    Trial1

--- run.py
+++ run.py
@@ -156,8 +156,8 @@ def main(trader):
 
 
 if __name__ == '__main__':
-    with shift.Trader("test001") as trader:
-        trader.connect("initiator.cfg", "password")
+    with shift.Trader("alt_test01") as trader:
+        trader.connect("initiator.cfg", "10.246.103.34")
         sleep(1)
         trader.sub_all_order_book()
         sleep(1)
